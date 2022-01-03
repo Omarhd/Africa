ed797fc2b284a9f06ae90b61b619c5e6bad74444
Omar <omeralhadu@gmail.com>
2022-01-03 20:50:11 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Omar Abdulrahman on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
